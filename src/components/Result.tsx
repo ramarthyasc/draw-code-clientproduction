2e2299f0e7e877809f8dcfd82b6d353eea780d16
R Amarthya Sreechand <amarthyasreechand@gmail.com>
2026-01-19 17:22:57 +0530

    Stringify result box objects

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,3 +1,24 @@
+function stringLogger(input: unknown) {
+    if (typeof input === "boolean" ||
+        typeof input === "number" ||
+        typeof input === "string" ||
+        Object.prototype.toString.call(input) === "[object Null]"
+    ) {
+        return `${input}`;
+    } else if (typeof input === "undefined") {
+        return `"undefined"`;
+    } else if (typeof input === "function") {
+        return `"function"`;
+    }
+    else if (
+        Object.prototype.toString.call(input) === "[object Array]" ||
+        Object.prototype.toString.call(input) === "[object Object]"
+    ) {
+        // json.stringify removes functions and undefined inside the array or object
+        return JSON.stringify(input);
+    }
+}
+
 export function Result({ result }: any) {
     if (!result) { return; };  // Need this
 
@@ -7,11 +28,11 @@ export function Result({ result }: any) {
     return (
         <div className="mx-2 text-gray-900">
             <div> Input: </div>
-            <div className="my-2 py-2 px-2 rounded-md bg-gray-200"> {resultObject.input} </div>
+            <div className="my-2 py-2 px-2 rounded-md bg-gray-200"> {stringLogger(resultObject.input)} </div>
             <div> My Output: </div>
-            <div className="my-2 py-2 px-2 rounded-md bg-gray-200"> {resultObject.userOutput} </div>
+            <div className="my-2 py-2 px-2 rounded-md bg-gray-200"> {stringLogger(resultObject.userOutput)} </div>
             <div> Expected Output: </div>
-            <div className="my-2 py-2 px-2 rounded-md bg-gray-200"> {resultObject.expOutput} </div>
+            <div className="my-2 py-2 px-2 rounded-md bg-gray-200"> {stringLogger(resultObject.expOutput)} </div>
             {userlogArray.length ?
                 <div>
                     <hr className="mt-6 mb-4 text-gray-400"/>
